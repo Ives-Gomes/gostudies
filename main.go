8c07a8ffbeb97317d7a33eb65a2a3ca0ac09510b
Ives Moreira <ivesmoreira3@gmail.com>
2026-03-14 12:26:23 -0300

    feat: add config initialization and logging functionality; create Opening schema

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/Ives-Gomes/gostudies/router"
+import (
+	"fmt"
+
+	"github.com/Ives-Gomes/gostudies/config"
+	"github.com/Ives-Gomes/gostudies/router"
+)
 
 func main() {
+	err := config.Init()
+	if err != nil {
+		fmt.Printf("Error initializing config: %v\n", err)
+		return
+	}
+
 	router.Initialize()
 }
